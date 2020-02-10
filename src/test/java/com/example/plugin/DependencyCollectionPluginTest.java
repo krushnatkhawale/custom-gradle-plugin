53ffc4303011ab2527e8de533f545588197930ec
krushnatkhawale@gmail.com <krushnatkhawale@gmail.com>
2020-02-10 22:13:23 +0000

    Test refactored

--- src/test/java/com/example/plugin/DependencyCollectionPluginTest.java
+++ src/test/java/com/example/plugin/DependencyCollectionPluginTest.java
@@ -4,6 +4,7 @@ import org.assertj.core.api.Assertions;
 import org.gradle.api.Project;
 import org.gradle.api.Task;
 import org.gradle.testfixtures.ProjectBuilder;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,27 +16,25 @@ public class DependencyCollectionPluginTest {
 
     private static Project project = setupProject();
 
-    private File settingsFile;
-    private File buildFile;
-
     @BeforeEach
     public void setUp() throws IOException {
+
+    }
+
+    @AfterEach
+    public void afterAll() throws IOException {
+        project.getTasks().getAt("clean").getActions();
     }
 
     @DisplayName("Basic test to test if correct task is run")
     @Test
-    public void mainTest() {
-        System.out.println("START");
+    public void mainTest() throws IOException {
         Task myGradleTask = project.getTasks().getAt("myGradleTask");
         Assertions.assertThat(myGradleTask).isInstanceOf(DependencyCollectionTask.class);
-        System.out.println("DONE");
     }
 
     private static Project setupProject() {
-        final Project project = ProjectBuilder.builder()
-                .withName("qa")
-                .withProjectDir(new File("build"))
-                .build();
+        final Project project = ProjectBuilder.builder().withName("qa").withProjectDir(new File("build")).build();
         project.setVersion("1.0.0");
         project.getPlugins().apply("java");
         project.getPlugins().apply("myGradleTask");
